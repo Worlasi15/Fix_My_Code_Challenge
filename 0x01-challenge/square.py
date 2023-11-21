240c4880c81e72026698a178bc60cc5698dcc16e
Worlasi15 <efyawalasi@gmail.com>
2023-11-21 13:12:39 -0800

    perfect square

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, **kwargs):
+        self.width = 0
+        self.height = 0
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def perimeter(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print("Area:", s.area())
+    print("Perimeter:", s.perimeter())
